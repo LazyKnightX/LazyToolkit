75cb10226f1f43acca827af9811399cd0b7c5aa1
Lazy Knight <i@lazyknight.com>
2018-04-14 08:05:26 +0800

    update toolkit

---
+++ Utility/ActionUtility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+namespace LazyToolkit
+{
+  public class RandomActionInfo
+  {
+    public float weight;
+    public Action action;
+
+    public RandomActionInfo(float weight, Action action)
+    {
+      this.weight = weight;
+      this.action = action;
+    }
+  }
+
+  public static class ActionUtility
+  {
+    /// <summary>
+    /// Execute random action by weight.
+    /// </summary>
+    /// <param name="actionInfos"></param>
+    public static void CallRandomAction(List<RandomActionInfo> actionInfos)
+    {
+      var totalWeight = actionInfos.Sum(info => info.weight);
+      var randomValue = Random.Range(0f, totalWeight);
+      var tick = 0f;
+      foreach (var actionInfo in actionInfos)
+      {
+        tick += actionInfo.weight;
+        if (randomValue <= tick)
+        {
+          actionInfo.action();
+          return;
+        }
+      }
+    }
+  }
+}
---
+++ Utility/MathUtility.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace LazyToolkit
+{
+  public class MathUtility
+  {
+    /// <summary>
+    /// Returns a random integer number between min [inclusive] and max [inclusive] (Read Only).
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public static int GetRandomInt(int min, int max)
+    {
+      return Random.Range(min, max + 1);
+    }
+
+    public static float GetRandomFloat(float min, float max)
+    {
+      return Random.Range(min, max);
+    }
+
+    public static Vector3 PolarCoordinateDisplacement(Vector3 origin, float radian, float distance)
+    {
+      float x = Mathf.Sin(radian) * distance;
+      float y = Mathf.Cos(radian) * distance;
+      origin.x += x;
+      origin.y += y;
+      return origin;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="successRate">0.00 ~ 1.00</param>
+    /// <returns></returns>
+    public static bool GetSuccessResult(float successRate)
+    {
+      float tick = GetRandomFloat(0, 1);
+      bool result = tick < successRate;
+      return result;
+    }
+  }
+}
