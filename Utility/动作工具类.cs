b8280ff462f379ac86a0f5d6188c17ef922477d5
Lazy Knight <i@lazyknight.com>
2018-04-30 14:28:17 +0800

    awsdasdawd

---
+++ Utility/动作工具类.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Random = UnityEngine.Random;
+
+namespace LazyToolkit
+{
+  public class 随机动作信息
+  {
+    public float 随机权重;
+    public Action 随机动作;
+
+    public 随机动作信息(float 随机权重, Action 随机动作)
+    {
+      this.随机权重 = 随机权重;
+      this.随机动作 = 随机动作;
+    }
+  }
+
+  public class 随机文本信息
+  {
+    public float 随机权重;
+    public string 随机文本;
+
+    public 随机文本信息(float 随机权重, string 随机文本)
+    {
+      this.随机权重 = 随机权重;
+      this.随机文本 = 随机文本;
+    }
+  }
+
+  public static class 动作工具类
+  {
+    /// <summary>
+    /// Execute random action by weight.
+    /// </summary>
+    /// <param name="随机动作信息列表"></param>
+    public static void 随机执行动作(List<随机动作信息> 随机动作信息列表)
+    {
+      var 总权重 = 随机动作信息列表.Sum(info => info.随机权重);
+      var 随机值 = Random.Range(0f, 总权重);
+      var 随机值检测点 = 0f;
+      foreach (var 随机动作信息 in 随机动作信息列表)
+      {
+        随机值检测点 += 随机动作信息.随机权重;
+        if (随机值 <= 随机值检测点)
+        {
+          随机动作信息.随机动作();
+          return;
+        }
+      }
+    }
+  }
+}
---
+++ Utility/数学工具类.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace LazyToolkit
+{
+  public class 数学工具类
+  {
+    /// <summary>
+    /// Returns a random integer number between min [inclusive] and max [inclusive] (Read Only).
+    /// </summary>
+    /// <param name="最小值"></param>
+    /// <param name="最大值"></param>
+    /// <returns></returns>
+    public static int 获取随机整数(int 最小值, int 最大值)
+    {
+      return Random.Range(最小值, 最大值 + 1);
+    }
+
+    public static float 获取随机浮点数(float 最小值, float 最大值)
+    {
+      return Random.Range(最小值, 最大值);
+    }
+
+    public static Vector3 极坐标位移(Vector3 原点, float 半径, float 距离)
+    {
+      float x = Mathf.Sin(半径) * 距离;
+      float y = Mathf.Cos(半径) * 距离;
+      原点.x += x;
+      原点.y += y;
+      return 原点;
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="successRate">0.00 ~ 1.00</param>
+    /// <returns></returns>
+    public static bool 判断成功率(float successRate)
+    {
+      float tick = 获取随机浮点数(0, 1);
+      bool result = tick < successRate;
+      return result;
+    }
+  }
+}
